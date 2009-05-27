fbbc26b38751422a3f97b5e1312c0b35438857ed
Denis Barushev <barushev@gmail.com>
2009-05-27 15:24:22 +0000

    Added search results.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,25 @@ $(
     function () {
         function updateSuggestions(data) {
             var jqSuggestions = $("#suggestions");
+            $("#results").html("");
+
+            $.get(
+                "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&hl=en&rsz=large&q=" + encodeURIComponent(data[1][0][0]),
+                {},
+                function (data) { updateResults(data) },
+                "jsonp"
+            );
 
             for (i in data[1]) {
                 jqSuggestions.append("<li>" + data[1][i][0] + "</li>");
+            }
+        }
+
+        function updateResults(data) {
+            var jqResults = $("#results");
+
+            for (i in data["responseData"]["results"]) {
+                jqResults.append("<li>" + data["responseData"]["results"][i]["title"] + ' <a href="' + data["responseData"]["results"][i]["visibleUrl"] + '">' + data["responseData"]["results"][i]["url"] + "</a></li>");
             }
         }
 
