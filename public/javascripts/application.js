551d29bff5fba8de1c5f23a80a8d74acbb1aebf6
Denis Barushev <barushev@gmail.com>
2009-05-27 14:56:31 +0000

    Created page with simple suggestions.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,26 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(
+    function () {
+        function updateSuggestions(data) {
+            var jqSuggestions = $("#suggestions");
+
+            for (i in data[1]) {
+                jqSuggestions.append("<li>" + data[1][i][0] + "</li>");
+            }
+        }
+
+        $("#q").keyup(
+            function () {
+                $("#suggestions").html("");
+
+                if (this.value) {
+                    $.get(
+                        "http://google.com/complete/search?q=" + encodeURIComponent(this.value),
+                        {},
+                        function (data) { updateSuggestions(data) },
+                        "jsonp"
+                    );
+                }
+            }
+        );
+    }
+);
