0638061cd6cebe17189c3647be7e118494b07929
Denis Barushev <barushev@gmail.com>
2009-06-02 23:08:44 +0300

    Added shadow to the thumbnails, links to the suggestions, query to the url.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,7 +17,7 @@ $(
 
             for (i = 0; i < 5; i++) {
                 if (data[1][i]) {
-                    jqSuggestions.append("<li>" + data[1][i][0] + "</li>");
+                    jqSuggestions.append('<li><a href="#' + encodeURIComponent(data[1][i][0]) + '">' + data[1][i][0] + "</a></li>");
                 }
             }
         }
@@ -27,7 +27,7 @@ $(
 
             for (i = 0; i < 3; i++) {
                 if (data["responseData"]["results"][i]) {
-                    jqResults.append('<li style="float: left; padding: 10px; list-style-type: none; margin: 0;"><img width="280" height="202" src="http://images.pageglimpse.com/v1/thumbnails?url=' + encodeURIComponent(data["responseData"]["results"][i]["url"]) + '&size=medium&devkey=8e97fc866d361db0b1d4e5a427a69136"><br>' + data["responseData"]["results"][i]["title"] + ' <a href="' + data["responseData"]["results"][i]["url"] + '">' + data["responseData"]["results"][i]["visibleUrl"] + "</a></li>");
+                    jqResults.append('<li><div class="thumbnail"><img width="280" height="202" src="http://images.pageglimpse.com/v1/thumbnails?url=' + encodeURIComponent(data["responseData"]["results"][i]["url"]) + '&size=medium&devkey=8e97fc866d361db0b1d4e5a427a69136"></div><div class="link">' + data["responseData"]["results"][i]["title"] + ' <a href="' + data["responseData"]["results"][i]["url"] + '">' + data["responseData"]["results"][i]["visibleUrl"] + "</a></div></li>");
                 }
             }
         }
@@ -35,6 +35,7 @@ $(
         $("#q").keyup(
             function () {
                 $("#suggestions").html("");
+                location.hash = this.value;
 
                 if (this.value) {
                     $.get(
@@ -46,5 +47,15 @@ $(
                 }
             }
         );
+
+        $("#suggestions a").live("click",
+            function () {
+                $("#q").val($(this).text()).keyup();
+            }
+        );
+
+        if (location.hash.length > 2) {
+            $("#q").val(location.hash.substr(1)).keyup();
+        }
     }
 );
